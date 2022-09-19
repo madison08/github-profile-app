74022c52d984031a5ee148a203087260fd3150ca
madison08 <madisonamoa@gmail.com>
2022-09-19 14:53:56 +0000

    login/logout

--- lib/providers/DataProvider.dart
+++ lib/providers/DataProvider.dart
@@ -6,11 +6,18 @@ import 'package:github_profile/api_const.dart';
 import 'package:github_profile/models/User.dart';
 import 'package:github_profile/models/http_exception.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DataProvider with ChangeNotifier {
   dynamic error;
 
   bool loadingLogin = false;
+
+  String? userConnected;
+
+  bool get isAuth {
+    return userConnected != null;
+  }
 
   Future<void> getUserLog(username) async {
     error = null;
@@ -26,6 +33,12 @@ class DataProvider with ChangeNotifier {
         case 200:
           print("user login");
           print(User.fromJson(data));
+
+          final prefs = await SharedPreferences.getInstance();
+
+          prefs.setString("username", username);
+          userConnected = username;
+
           break;
         case 404:
           print("ENTER IN REQUEST");
@@ -52,5 +65,21 @@ class DataProvider with ChangeNotifier {
     loadingLogin = false;
 
     notifyListeners();
+  }
+
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    if (!prefs.containsKey('username')) {
+      return false;
+    }
+
+    return true;
+  }
+
+  Future<void> logout() async {
+    final prefs = await SharedPreferences.getInstance();
+
+    prefs.clear();
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -3,7 +3,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:github_profile/providers/DataProvider.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -13,6 +16,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool logoutLoder = false;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -40,17 +45,56 @@ class _HomeScreenState extends State<HomeScreen> {
           ],
         ),
         body: Center(
-          child: Text(
-            "Hello les Giters !",
-            style: GoogleFonts.shareTechMono(
-              fontSize: 35.0,
-              fontWeight: FontWeight.w900,
-              letterSpacing: -4,
-              color: Colors.white,
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Hello les Giters !",
+                style: GoogleFonts.shareTechMono(
+                  fontSize: 35.0,
+                  fontWeight: FontWeight.w900,
+                  letterSpacing: -4,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                ),
+                onPressed: () {
+                  _logout();
+                },
+                child: Text("Deconexion"),
+              )
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void _logout() async {
+    setState(() {
+      logoutLoder = true;
+    });
+    await Provider.of<DataProvider>(context, listen: false).logout();
+
+    setState(() {
+      logoutLoder = false;
+    });
+
+    Fluttertoast.showToast(
+        msg: "Deconnecter",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0);
+
+    Navigator.pushNamedAndRemoveUntil(context, "/login", (route) => false);
   }
 }
